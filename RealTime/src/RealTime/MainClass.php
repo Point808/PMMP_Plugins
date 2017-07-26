730d79cdb5a09ed7c4dc5cfd563ded33765f913a
Josh North <josh.north@donotreply@point808.com>
2017-07-26 09:37:03 -0400

    Update 'RealTime/src/RealTime/MainClass.php'

--- RealTime/src/RealTime/MainClass.php
+++ RealTime/src/RealTime/MainClass.php
@@ -93,7 +93,7 @@ $test = date('H') + ($modify);
 	
 	
 	
-	public function onCommand(CommandSender $sender, Command $command, string $label, array $args){
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
 		
 		if(!($sender instanceof Player)){
 		$this->getLogger()->info(TextFormat::DARK_RED. "[RealTime] Please run this command in-game.\n");						
